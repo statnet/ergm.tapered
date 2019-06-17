a816625f229810959a915d9aece9ae73315d0237
Mark S. Handcock <handcock@stat.ucla.edu>
2019-06-17 12:38:09 -0700

    Fixed bug in "taper.terms" that mangled the necessary formula. Added a "tau" option to "ergm.tapered" that allowed direct specification of the (negative) of the natural parameter of the tapering term(s).

--- R/ergm.tapered.R
+++ R/ergm.tapered.R
@@ -1,7 +1,8 @@
 #' Fits a Tapered ERGM
 #' @param formula An ergm formula to fit
-#' @param r The scaling factor to use for the hueristic of setting beta equal to r the standard deviation of the observed statistics
-#' @param beta The tapering parameters. If not null, these override the hueristics.
+#' @param r The scaling factor to use for the hueristic of setting beta equal to r standard deviations of the observed statistics
+#' @param beta The tapering parameters, expressed as in Fellows and Handcock (2017). If not NULL, these override the hueristics (r).
+#' @param tau The tapering parameters, expressed as natural parameters. If not NULL, these override the beta and the hueristics (r).
 #' @param tapering.centers The centers of the tapering terms. If null, these are taken to be the mean value parameters.
 #' @param control An object of class control.ergm. Passed to the ergm function.
 #' @param ... Additional arguments to ergm.
@@ -19,7 +20,7 @@
 #' summary(fit)
 #' }
 #' @export
-ergm.tapered <- function(formula, r=2, beta=NULL, tapering.centers=NULL, target.stats=NULL,
+ergm.tapered <- function(formula, r=2, beta=NULL, tau=NULL, tapering.centers=NULL, target.stats=NULL,
 			 family="taper", taper.terms="dependent",
                          control = control.ergm(MCMLE.termination="confidence"), ...){
   
@@ -43,56 +44,74 @@ ergm.tapered <- function(formula, r=2, beta=NULL, tapering.centers=NULL, target.
     ostats <- tapering.centers
   
   # set tapering coefficient
-  coef <- switch(family,
+  tau <- switch(family,
     "stereo"={
-      if(is.null(beta)){
+      if(is.null(beta) & is.null(tau)){
         1
       }else{
+       if(is.null(tau)){
         beta
+       }else{
+        tau
+       }
       }},
-      {if(is.null(beta)){
+      {if(is.null(tau)){
+      if(is.null(beta)){
         1 / (r^2 * pmax(1,abs(ostats[taper.terms])))
       }else{
         1 / beta^2
-      }}
+      }}else{tau}}
   )
   npar <- length(ostats)
-  names(coef) <- names(ostats)[taper.terms]
+  names(tau) <- names(ostats)[taper.terms]
   
-  terms <- list_rhs.formula(formula)
-  terms[!taper.terms] <- NULL
-  attr(terms,"sign") <- attr(terms,"sign")[taper.terms]
-  taper_formula=append_rhs.formula(~.,terms)
-  taper_formula <- switch(family,
-    "stereo"=statnet.common::nonsimp_update.formula(taper_formula,.~Stereo(~.,coef=.taper.coef,m=.taper.center),
-              environment(), from.new=TRUE), 
-             statnet.common::nonsimp_update.formula(taper_formula,.~Taper(~.,coef=.taper.coef,m=.taper.center),
-              environment(), from.new=TRUE) 
-	     )
+  if(any(!taper.terms)){
+    terms <- list_rhs.formula(formula)
+    terms[!taper.terms] <- NULL
+    attr(terms,"sign") <- attr(terms,"sign")[taper.terms]
+    taper_formula=append_rhs.formula(~.,terms)
+    taper_formula <- switch(family,
+      "stereo"=statnet.common::nonsimp_update.formula(taper_formula,.~Stereo(~.,coef=.taper.coef,m=.taper.center),
+                environment(), from.new=TRUE), 
+               statnet.common::nonsimp_update.formula(taper_formula,.~Taper(~.,coef=.taper.coef,m=.taper.center),
+                environment(), from.new=TRUE) 
+	       )
 # taper_formula=statnet.common::nonsimp_update.formula(taper_formula,.~Taper(~.,coef=.taper.coef,m=.taper.center))
 
-  trimmed_formula <- statnet.common::filter_rhs.formula(formula, function(x){
-   ans <- FALSE
-   if(is.call(x) ){
-	  for(i in seq_along(terms)){
-	   if(length(x)==length(terms[[i]])){
-	     ans <- !ans & x==terms[[i]]
-	   }
-	  }
-   }
-   !ans
-  })
   # do some formula magic
+#  trimmed_formula <- statnet.common::filter_rhs.formula(formula, function(x, ...){
+#   ans <- rep(FALSE,length(x))
+#   if(is.call(x) ){
+#    for(j in seq_along(x)){
+#     for(i in seq_along(terms)){
+#      if(length(x[[j]])==length(terms[[i]])){
+#       ans[j] <- !ans[j] & x[[j]]==terms[[i]]
+#      }
+#     }
+#    }
+#   }
+#   !ans
+#   }, terms)
+   trimmed_formula <- filter_rhs.formula(formula, `!=`, terms)
 
-  if(is.null(target.stats)){
    newformula <- append_rhs.formula(trimmed_formula,taper_formula, environment()) 
   }else{
-   newformula <- append_rhs.formula(trimmed_formula,taper_formula, environment()) 
+   newformula <- switch(family,
+      "stereo"=statnet.common::nonsimp_update.formula(formula,.~Stereo(~.,coef=.taper.coef,m=.taper.center),
+                environment(), from.new=TRUE), 
+               statnet.common::nonsimp_update.formula(formula,.~Taper(~.,coef=.taper.coef,m=.taper.center),
+                environment(), from.new=TRUE) 
+	       )
   }
   env <- new.env(parent=environment(formula))
   env$.taper.center <- ostats[taper.terms]
-  env$.taper.coef <- coef
+  env$.taper.coef <- tau
   environment(newformula) <- env
+
+  message("The (natural) tapering parameters are:")
+  for(i in seq_along(tau)){
+    message(sprintf(" %s : %f",names(tau)[i],tau[i]))
+  }
   
   if(control$MCMLE.termination == "Hotelling") control$MCMLE.termination <- "confidence"
 
@@ -100,14 +119,14 @@ ergm.tapered <- function(formula, r=2, beta=NULL, tapering.centers=NULL, target.
   if(is.null(target.stats)){
     fit <- ergm(newformula, control=control, ...)
   }else{
-    fit <- ergm(newformula, control=control, target.stats=ostats, offset.coef=coef, ...)
+    fit <- ergm(newformula, control=control, target.stats=ostats, offset.coef=tau, ...)
   }
   
   
   # post processs fit to alter Hessian etc
   sample <- fit$sample[[1]][,1:npar,drop=FALSE]
   if(is.null(tapering.centers)){
-    hess <- .tapered.hessian(sample, coef)
+    hess <- .tapered.hessian(sample, tau)
     if(is.curved(fit)){
       curved_m <- ergm_model(formula)
       curved_m <- .tapered.curved.hessian(hess,fit$coef,curved_m$etamap)
@@ -118,7 +137,7 @@ ergm.tapered <- function(formula, r=2, beta=NULL, tapering.centers=NULL, target.
     fit$covar <- -MASS::ginv(fit$hessian)
   }
   fit$tapering.centers <- ostats[taper.terms]
-  fit$tapering.coef <- coef
+  fit$tapering.coef <- tau
   fit$orig.formula <- formula
   class(fit) <- c("tapered.ergm",family,class(fit))
   
