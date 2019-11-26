fbd7013d7889e9a4aa179c8bf7b7d205dcf2bece
Mark S. Handcock <handcock@stat.ucla.edu>
2019-11-25 17:38:22 -0800

    Finish conversion to dependence on "ergd" (over "ergm")

--- R/ergm.mvtapered.R
+++ R/ergm.mvtapered.R
@@ -121,7 +121,7 @@ ergm.mvtapered <- function(formula, r=2, mv=NULL, tapering.centers=NULL,
   message("\n")
   
   # fit ergm
-  fit <- ergm(newformula, target.stats=target.stats, control=control,...)
+  fit <- ergd(newformula, target.stats=target.stats, control=control,...)
   
 # # post processs fit to alter Hessian etc
 # sample <- fit$sample[[1]][,1:npar,drop=FALSE]
