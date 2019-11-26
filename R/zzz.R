322b9ac917b4b5b15768190a5a0bc3c9f45e6848
Mark S. Handcock <handcock@ucla.edu>
2019-11-25 19:08:19 -0800

    Unload ergm name space on load (if preference to "ergd").

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
 .onUnload <- function(libpath){
   library.dynam.unload("ergm.tapered",libpath)
 }
+.onAttach <- function(lib, pkg){
+  if("package:ergm" %in% search()){
+    message("Detaching prior version of 'ergm'.")
+    detach("package:ergm", unload=TRUE)
+  }
+}
