c78d8c0ff50377ab0f4508ef8100770a8edcf953
handcock <handcock@ucla.edu>
2022-08-31 18:01:33 -0700

    Added documentation
    Added documentation for InitErgmTerm* and a general ergm-terms-taper.

--- R/InitErgmTerm.Kpenalty.R
+++ R/InitErgmTerm.Kpenalty.R
@@ -1,3 +1,27 @@
+################################################################################
+
+#' @templateVar name Kpenalty
+#' @title Kpenalty 
+#' @description 
+#'    Adds \emph{only} the quadratic penalty of Fellows and Handcock
+#'    (2017) for the terms specified in \code{formula} to the model.
+#' @usage
+#' # binary: Kpenalty(formula=NULL, coef=NULL, m=NULL)
+#'
+#' @param formula a valid formula for a  standard ERGM
+#' @param coef a numeric vector of coefficients giving the penalty coefficients \eqn{\beta} for the tapering of the terms.
+#'   If \code{NULL} is passed, the tapering coefficients are set to \code{1/(4*summary(formula))}, the default
+#'   in Fellows and Handcock (2017).
+#'   If a numeric vector is given, there are interpreted as the tapering coefficients of the terms in the
+#'   model, including the terms enclosed in \code{offset()}.
+#'   If a numeric scalar is given, it is interpreted as a multiplier of the default tapering coefficients , that is,
+#' @param m numeric vector. If given, is
+#'    the value of the network statistic relative to which the model is
+#'    tapered. If omitted, it defaults to that of the model's LHS
+#'    network if \code{formula} is one-sided and that of the network on
+#'    the LHS of \code{formula} if it is two-sided.
+#'
+#' @concept tapered
 #' @import ergm statnet.common network
 InitErgmTerm.Kpenalty <- function(nw, arglist, response=NULL, ...){
   a <- check.ErgmTerm(nw, arglist,
--- R/InitErgmTerm.M4.R
+++ R/InitErgmTerm.M4.R
@@ -1,3 +1,28 @@
+################################################################################
+
+#' @templateVar name M4
+#' @title M4 
+#' @description 
+#'    Adds the fourth moment of each statistics specified by the terms in \code{formula} to the model. 
+#'    This is sometimes applied directly to control the fourth moment of the standard
+#'    statistics, but more typically as part of a Tapered ERGM.
+#' @usage
+#' # binary: M4(formula=NULL, coef=NULL, m=NULL)
+#'
+#' @param formula a valid formula for a  standard ERGM
+#' @param coef a numeric vector of coefficients giving the penalty coefficients \eqn{\beta} for the tapering of the terms.
+#'   If \code{NULL} is passed, the tapering coefficients are set to \code{1/(4*summary(formula))}, the default
+#'   in Fellows and Handcock (2017).
+#'   If a numeric vector is given, there are interpreted as the tapering coefficients of the terms in the
+#'   model, including the terms enclosed in \code{offset()}.
+#'   If a numeric scalar is given, it is interpreted as a multiplier of the default tapering coefficients , that is,
+#' @param m numeric vector. If given, is
+#'    the value of the network statistic relative to which the model is
+#'    tapered. If omitted, it defaults to that of the model's LHS
+#'    network if \code{formula} is one-sided and that of the network on
+#'    the LHS of \code{formula} if it is two-sided.
+#'
+#' @concept tapered
 #' @import ergm statnet.common network
 InitErgmTerm.M4 <- function(nw, arglist, response=NULL, ...){
   a <- ergm::check.ErgmTerm(nw, arglist,
